d267f8aad8ad064aa9c005d905e5481bdfd9f748
David Seto <67933197+dtseto@users.noreply.github.com>
2025-07-18 17:53:14 -0700

    Initial Commit

---
+++ ExhaleGUIApp/ExhaleGUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExhaleGUIAppApp.swift
+//  ExhaleGUIApp
+//
+//  Created by User2 on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ExhaleGUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExhaleGUIAppTests/ExhaleGUIAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  ExhaleGUIAppTests.swift
+//  ExhaleGUIAppTests
+//
+//  Created by User2 on 7/18/25.
+//
+
+import Testing
+
+struct ExhaleGUIAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
